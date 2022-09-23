1a65f42a461cab62d984b3dc299f5a194575c48f
yutoi-git <yuto.i0912@gmail.com>
2022-09-24 00:11:24 +0900

    Add: 取得した情報を追加

--- db/fixtures/development/3_station_csv.rb
+++ db/fixtures/development/3_station_csv.rb
@@ -8,6 +8,8 @@ csv.each do |line|
   name = line[2]
   longitude = line[3]
   latitude = line[4]
+  lower_six = line[5]
+  upper_six = line[6]
 
   Station.seed(:station_code) do |s|
     s.station_code = station_code
@@ -15,5 +17,7 @@ csv.each do |line|
     s.name = name
     s.longitude = longitude
     s.latitude = latitude
+    s.lower_six = lower_six
+    s.upper_six = upper_six
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_22_031641) do
+ActiveRecord::Schema.define(version: 2022_09_23_080254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2022_09_22_031641) do
     t.float "latitude", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "lower_six"
+    t.float "upper_six"
   end
 
   add_foreign_key "lines", "companies", column: "company_code", primary_key: "company_code"
