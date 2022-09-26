99dc418d9ad03d23b6d5b922afc3d7e8183e5f8d
yutoi-git <yuto.i0912@gmail.com>
2022-09-26 23:47:36 +0900

    Fix: 駅と対応する路線を取得できるようにモデルの修正

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,2 +1,10 @@
 class Station < ApplicationRecord
+  self.primary_key = :station_code
+
+  belongs_to :company, foreign_key: :company_code
+  has_many :lines_stations, foreign_key: :station_code
+  has_many :lines, through: :lines_stations
+
+  validates :station_code, presence: true, uniqueness: true
+  validates :name, presence: true
 end
